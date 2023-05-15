957e592c0de9a7d7538b986bd57d9455578475f4
Kalybaev Rustam <me@kalybaev.ru>
2023-05-16 00:38:53 +0500

    task 43 done

---
+++ task43/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения: ({x}, {y})");
